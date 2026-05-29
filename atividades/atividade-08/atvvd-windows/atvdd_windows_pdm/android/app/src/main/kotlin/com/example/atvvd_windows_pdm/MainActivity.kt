bd7d687311a4676521d9565026420029bb04475c
Davi <davicostta54@gmail.com>
2026-05-28 23:00:15 -0300

    atividade-08

---
+++ atividades/atividade-08/atvvd-windows/atvdd_windows_pdm/android/app/src/main/kotlin/com/example/atvvd_windows_pdm/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.atvvd_windows_pdm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ atividades/atividade-08/atvvd-windows/atvdd_windows_pdm/lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = 'Ov23liqA76S7Poa6zRq4';
+const githubClientSecret = '8ee843ca5766e3fd7723b3e58982684d014a98fd';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
